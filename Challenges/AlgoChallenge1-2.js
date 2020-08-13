f10ae52b126b3c52dabc7fc3a4581608f754fba5
piyushkochhar <piyush.kochhar1@gmail.com>
2020-08-13 03:30:33 -0500

    Updated AlgoChallenge1-2

--- Challenges/AlgoChallenge1-2.js
+++ Challenges/AlgoChallenge1-2.js
@@ -1,23 +1,23 @@
-/*
-
-Update AlgoChallenge 1 to include error detection
-
-(e) Add error detection using checksum
-
-*/
-
 let ascii = {};
 
+//Create an ASCII obejct
+//Output: Object { char: decimal_value}
 function makeAscii(){
   for(let i = 0; i < 128; i++){
     ascii[String.fromCharCode(i)] = i
   }
 }
 
+//Converts string into it's decimal equivalent
+//Input: Binary string
+//Output: Array of Decimal values for each charcater
 function getDecimal(str){
   return str.split('').map((val) => ascii[val]);
 }
 
+//Converts decimal into binary
+//Input: Decimal number
+//Output: 8-bit binary string
 function binary(num){
   let result = '';
 
@@ -35,6 +35,9 @@ function binary(num){
 
 }
 
+//Pads 0's to binary string 
+//Input: Binary string
+//Output: 8-bit binary string
 function addPadding(str){
   let length = str.length;
   
@@ -46,12 +49,18 @@ function addPadding(str){
 
 }
 
+//Get Binaries for all decimals values
+//Input: ASCII String
+//Output: Array of all binaries
 function getBinaries(str){
   return getDecimal(str).map((val) => {
     return binary(val)
     })
 }
 
+//Adds two 8-bit binaries
+//Input: Takes 2 8-bit binary strings
+//Output: Truncated 8-bit binary string
 function addBinary(num1,num2){
   let carry = 0;
   let result = '';
@@ -95,10 +104,16 @@ function addBinary(num1,num2){
   return result;
 }
 
+//Adds two 8-bit binaries
+//Input: Takes 2 8-bit binary strings
+//Output: Truncated 8-bit binary string
 function getSum(binaries){
   return binaries.reduce((prev,curr) => truncateBinary(addBinary(prev,curr)))
 }
 
+//Add extra bits to the rightmost 8 binary bits
+//Input: 8-bit binary string
+//Output: 8-bit binary string with added bits
 function truncateBinary(str){
   let extraBits = str.length - 8;
   if(extraBits > 0){
@@ -107,19 +122,20 @@ function truncateBinary(str){
   return str
 }
 
-function checksum(str){
-  return str.split('').map((num,i) => {
+//Performs 1's complement of the 8-bit binary
+//Input: Sum of all binaries
+//Output: Inverse of 8-bit binary string
+function checksum(sum){
+  return sum.split('').map((num,i) => {
     if(num === '0') return '1';
     return '0'
   }).join('')
 }
 
-function getBinaries(str){
-  return getDecimal(str).map((val) => {
-    return binary(val)
-    })
-}
-
+//Sends the data
+//Input: ASCII String
+//Output: Array of checksum at 0th position followed by
+//8-bit binary equivalent string values of each character
 function sender(str){
   let binaries = getBinaries(str);
   let chsum = checksum(getSum(binaries));
@@ -128,6 +144,9 @@ function sender(str){
   return data
 }
 
+//Checks the data sent by sender
+//Input: Data sent by sender (Array of checksum & 8-bit binaries)
+//Output: Sum of all the binaries in input array
 function receiver(data){
   console.log("Received...\n",data);
   
