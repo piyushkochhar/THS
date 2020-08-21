b28b78ef78a08701468f7aa49c3810eb39feec90
piyushkochhar <piyush.kochhar1@gmail.com>
2020-08-21 09:52:34 -0500

    Optimized Minesweeper

--- Challenges/Challenge 3 - Minesweeper/script.js
+++ Challenges/Challenge 3 - Minesweeper/script.js
@@ -1,55 +1,111 @@
 const app = document.getElementById('app');
 
-app.innerHTML = `<table></table>
-                <button class="reset" onClick="reset()">Reset</button>`;
+app.innerHTML = `<h3><img src="https://static.wixstatic.com/media/fef1e4_141cd29fc9624b23a4d2ad922dbcb149~mv2.png/v1/fill/w_220,h_220,al_c,q_85,usm_0.66_1.00_0.01/trophy.webp" class="trophy-head"/> : <span id="trophies"></span></h3>
+                <table></table>
+                <button class="reset" onClick="reset()">Reset</button>
+                <audio src="https://felgo.com/web-assets/pop.wav"></audio>`;
 
 const table = app.querySelector('table');
+const h3 = app.querySelector('h3');
+const trophies = app.querySelector('#trophies');
+const btn = app.querySelector('.reset');
+const audio = app.querySelector('audio');
 
-let state = Array(10)
-  .fill(0)
-  .map((val, i) => Array(10).fill(0));
+let state = {
+  data: [],
+  trophies: null,
+  bombs: [],
+};
 
-function initializeState() {
-  for (let i = 0; i < 100; i += 10) {
-    for (let j = 0; j < 10; j++) {
-      state[i / 10][j] = i + j + 1;
-    }
+function initState() {
+  for (let i = 0; i < 100; i++) {
+    let value = getRandom();
+    let isBomb = false;
+    state.data[i] = {
+      value,
+      isBomb,
+    };
   }
+  state.trophies = 0;
+  state.bombs = Array.from({ length: 3 }).map((val) => getRandom());
 }
 
-function findMultiple(num) {
-  state = state.map((row, i) => {
-    return row.map((val, i) => {
-      if (val % num === 0) {
-        return '**';
-      }
-      return val;
-    });
+function getRandom() {
+  return Math.floor(Math.floor(Math.random() * (100 - 1) + 1));
+}
+
+function findMultiples(num) {
+  audio.play();
+  state.data.forEach((data) => {
+    // if (findBomb(num) && num === data.value) {
+    //   data.isBomb = true;
+    // }
+    if (data.value % num === 0 && !findBomb(data.value)) {
+      state.trophies++;
+      data.value = '**';
+    } else if (data.value % num === 0 && findBomb(data.value)) {
+      data.isBomb = true;
+    }
   });
 
   render();
+}
+
+function findBomb(num) {
+  return state.bombs.includes(num);
 }
 
 function render() {
   let data = '';
+  let value = null;
+  state.data.forEach((val, i) => {
+    if (val.value === '**') {
+      value = `<td><img src="https://static.wixstatic.com/media/fef1e4_141cd29fc9624b23a4d2ad922dbcb149~mv2.png/v1/fill/w_220,h_220,al_c,q_85,usm_0.66_1.00_0.01/trophy.webp" class="trophy-img"/></td>`;
+    } else if (val.isBomb) {
+      value = `<td><img src="https://media.giphy.com/media/l378c6HXBGxfyF92E/giphy.gif" class="bomb-img"/></td>`;
+      stylesOnBomb();
+    } else {
+      value = `<td onClick='findMultiples(${val.value})'>${val.value}</td>`;
+    }
 
-  state.forEach((row) => {
-    data += `<tr>`;
-    row.forEach((val) => {
-      data += `<td onClick='findMultiple(${val})'>${val}</td>`;
-    });
-    data += `</tr>`;
+    if ((i + 1) % 10 === 1) {
+      data += '<tr>';
+      data += value;
+    } else if ((i + 1) % 10 === 0) {
+      data += value;
+      data += '</tr>';
+    } else {
+      data += value;
+    }
   });
+
   table.innerHTML = data;
+  trophies.innerText = state.trophies;
+}
+
+function stylesOnBomb() {
+  btn.innerText = 'Start Over';
+  btn.style.backgroundColor = '#cf1b1b';
+  h3.innerText = `You Lost! Try Again `;
+}
+
+function resetStyles() {
+  btn.innerText = 'Reset';
+  btn.style.backgroundColor = '#00d4ff';
+  h3.innerHTML = `<img src="https://static.wixstatic.com/media/fef1e4_141cd29fc9624b23a4d2ad922dbcb149~mv2.png/v1/fill/w_220,h_220,al_c,q_85,usm_0.66_1.00_0.01/trophy.webp" class="trophy-head"/> : <span id="trophies">${state.trophies}</span>`;
 }
 
 function reset() {
   init();
+  resetStyles();
 }
 
 function init() {
-  initializeState();
+  // O(n)
+  initState();
+  // O(n)
   render();
+  console.log(state.bombs);
 }
 
 init();
